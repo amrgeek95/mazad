0d851302752c9d284ed85aec9bb4ae0803c3dda7
amr sobhy <amr.geek.95@gmail.com>
2018-04-25 14:02:36 +0200

    Initial Commit

---
+++ Mazad/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mazad
+//
+//  Created by amr sobhy on 4/25/18.
+//  Copyright © 2018 amr sobhy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
