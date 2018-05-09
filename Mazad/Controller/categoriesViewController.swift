6d59ccb5ff87204ceeaa53cf103ecee89f5b2c8a
amr <amr.geek.95@gmail.com>
2018-05-09 07:46:07 +0200

    some changes

---
+++ Mazad/Controller/categoriesViewController.swift
@@ -0,0 +1,35 @@
+//
+//  categoriesViewController.swift
+//  Mazad
+//
+//  Created by amr sobhy on 4/25/18.
+//  Copyright © 2018 amr sobhy. All rights reserved.
+//
+
+import UIKit
+
+class categoriesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Mazad/View/addCommentTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  addCommentTableViewCell.swift
+//  Mazad
+//
+//  Created by amr sobhy on 4/29/18.
+//  Copyright © 2018 amr sobhy. All rights reserved.
+//
+
+import UIKit
+
+class addCommentTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
